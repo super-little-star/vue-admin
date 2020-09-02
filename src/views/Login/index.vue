fa44956f44f9f4749acf850f862bbb4e330297b4
super-little-star <superlittlestar114@email.com>
2020-09-03 05:15:09 +0800

    添加路由，引入ElementUI

---
+++ src/views/Login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-button>默认按钮</el-button>
+</template>
+<script>
+export default {
+  name: "login"
+};
+</script>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background: #344a5f;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/production-sub-path/" : "/",
-
   css: {
     loaderOptions: {
       sass: {
         prependData: '@import "./src/styles/main.scss";'
       }
     }
-  },
-  devServer: {
-    proxy: "http://localhost:4000"
   }
 };
